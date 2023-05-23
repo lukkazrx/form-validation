ff1d19b6f83491d435c7ae3017d5fa6540b9904c
Samuel Bittencourt <bittencourtlry@gmail.com>
2023-05-23 17:05:30 -0300

    email-validation

---
+++ index.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form')
+const compos = document.querySelectorAll('.required')
+const spans = document.querySelectorAll('.span')
+function validacaoEmail(){
+    let emailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    if(inputText.value.match(emailFormat))
+	{
+		alert("Esse não um email válido");
+		return false;
+		}
+}
